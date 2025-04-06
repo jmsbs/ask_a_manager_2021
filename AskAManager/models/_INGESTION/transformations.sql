dc5cfed62a6fb9d012d255e38084b336bda74542
jmsbs <jm.sbs@outlook.com>
2025-04-06 10:33:39 +0100

    Worked on country column cleaning.

--- AskAManager/models/_INGESTION/transformations.sql
+++ AskAManager/models/_INGESTION/transformations.sql
@@ -2,7 +2,19 @@
     materialized='view'
 ) }}
 
+-- Works with the raw_data table.
 SELECT 
+    MD5(
+        CONCAT_WS(
+            '||',
+            STRPTIME('4/27/2021 11:02:10', '%m/%d/%Y %H:%M:%S'),
+            age,
+            industry,
+            job_title,
+            state,
+            annual_salary
+        )
+    ) AS response_id,
     STRPTIME('4/27/2021 11:02:10', '%m/%d/%Y %H:%M:%S') AS timestamp, 
     CAST(age AS VARCHAR(10)) AS age,
     CAST(industry AS VARCHAR(200)) AS industry,
@@ -13,7 +25,19 @@ SELECT
     CAST(currency AS VARCHAR(6)) AS currency,
     CAST(other_currency AS VARCHAR) AS other_currency, -- needs treatment 
     CAST(income_context AS VARCHAR) AS income_context, -- needs treatment
-    CAST(country AS VARCHAR) AS country, -- needs treatment 
+    CAST(country AS VARCHAR) AS country,
+    CASE 
+        WHEN 
+            LOWER(REPLACE(REPLACE(country, ' ', ''), '.', '')) LIKE 'uniteds%' 
+            OR LOWER(REPLACE(REPLACE(country, ' ', ''), '.', '')) LIKE 'us' 
+            OR LOWER(REPLACE(REPLACE(country, ' ', ''), '.', '')) LIKE 'us%'
+            OR LOWER(REPLACE(REPLACE(country, ' ', ''), '.', '')) LIKE 'uni%stat%' THEN 'US'
+        WHEN 
+            LOWER(REPLACE(REPLACE(country, ' ', ''), '.', '')) LIKE 'unitedk%' 
+            OR LOWER(REPLACE(REPLACE(country, ' ', ''), '.', '')) LIKE 'uni%k%' 
+            OR LOWER(REPLACE(REPLACE(country, ' ', ''), '.', '')) LIKE 'uk%' THEN 'UK'
+        ELSE country
+    END AS f_country,
     state,
     city,
     year_of_experience as years_of_experience,
--- tasklist.md
+++ tasklist.md
@@ -1,4 +1,20 @@
-- [ ] Fix currency column
+- [x] Create ID column
+- [ ] Fix country column - WIP
+- [ ] Fix other_currency column
+- [ ] Fix additional_context column
+- [ ] Fix state column
+- [ ] Fix city column
 
-All values are populated with "If ""Other,"" please indicate the currency here:"
 
+
+
+Will have to create another state table.
+-- -- Creates the state table
+-- {{ config(
+--     materialized='table',
+--     alias='d.State'
+-- ) }}
+
+-- SELECT 
+--     response_id,
+--     state
