8fef21f6ff720ad9fcf16859ee963d6ebcf7878e
jmsbs <jm.sbs@outlook.com>
2025-04-06 08:22:46 +0100

    Worked on transformations.

--- AskAManager/models/_INGESTION/transformations.sql
+++ AskAManager/models/_INGESTION/transformations.sql
@@ -1,24 +1,41 @@
 {{ config(
     materialized='view'
 ) }}
-SELECT 
-    cast("Timestamp" AS timestamp) AS timestamp,
-    cast("How old are you?" AS age) as VARCHAR(10),
-    CAST("What industry do you work in?" AS industry) as VARCHAR(200),
-    CAST("Job title" AS job_title) AS varchar(150),
-    CAST("If your job title needs additional context, please clarify here:" AS job_title_context) as VARCHAR(1000),
-    CAST(replace(annual_salary, ',', '') "What is your annual salary? (You'll indicate the currency in a later question. If you are part-time or hourly, please enter an annualized equivalent -- what you would earn if you worked the job 40 hours a week, 52 weeks a year.)" AS annual_salary) as BIGINT,
-    CAST("How much additional monetary compensation do you get, if any (for example, bonuses or overtime in an average year)? Please only include monetary compensation here, not the value of benefits." AS additional_compensation) as INT,
-    CAST("Please indicate the currency" AS currency) as Varchar(6),
-    'If ""Other,"" please indicate the currency here:' AS other_currency,
-    "If your income needs additional context, please provide it here:" AS income_context,
-    "What country do you work in?" AS country,
-    "If you're in the U.S., what state do you work in?" AS state,
-    "What city do you work in?" AS city,
-    "How many years of professional work experience do you have overall?" AS total_experience_years,
-    "How many years of professional work experience do you have in your field?" AS field_experience_years,
-    "What is your highest level of education completed?" AS education_level,
-    "What is your gender?" AS gender,
-    "What is your race? (Choose all that apply.)" AS race
-FROM {{ ref('raw_data') }}
 
+SELECT 
+    STRPTIME('4/27/2021 11:02:10', '%m/%d/%Y %H:%M:%S') AS timestamp, 
+    CAST(age AS VARCHAR(10)) AS age,
+    CAST(industry AS VARCHAR(200)) AS industry,
+    CAST(job_title AS VARCHAR(150)) AS job_title,
+    CAST(job_title_context AS VARCHAR(1000)) AS job_title_context,
+    CAST(REPLACE(annual_salary, ',', '') AS BIGINT) AS annual_salary,
+    CAST(additional_compensation AS INT) AS additional_compensation,
+    CAST(currency AS VARCHAR(6)) AS currency,
+    CAST(other_currency AS VARCHAR) AS other_currency, -- needs treatment 
+    CAST(income_context AS VARCHAR) AS income_context, -- needs treatment
+    CAST(country AS VARCHAR) AS country, -- needs treatment 
+    state,
+    city,
+    year_of_experience as years_of_experience,
+    years_in_field,
+    highest_education,
+    CASE 
+        WHEN highest_education = 'High School' THEN 1
+        WHEN highest_education = 'College degree' THEN 2
+        WHEN highest_education = 'Master''s degree' THEN 3
+        WHEN highest_education = 'PhD' THEN 4
+        WHEN highest_education = 'Professional degree (MD, JD, etc.)' THEN 5
+        WHEN highest_education = 'Some degree' THEN 6
+        ELSE 0
+    END AS highest_education_encoded,
+    gender,
+    CASE 
+        WHEN gender = 'Non-binary' THEN 1
+        WHEN gender = 'Man' THEN 2
+        WHEN gender = 'Woman' THEN 3
+        WHEN gender = 'Other or prefer not to answer' THEN 4
+        WHEN gender = 'Prefer not to answer' THEN 5
+        ELSE 0
+    END AS gender_encoded,
+    race -- needs treatment
+FROM {{ ref('raw_data') }}
